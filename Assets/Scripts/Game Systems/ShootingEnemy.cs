283e0d7f0eae202d754f18aeea1b409b64708e08
Smug <zikadika2@gmail.com>
2024-06-27 13:29:06 -0400

    Updated look of shooting enemy and fixed ai for it

--- Assets/Scripts/Game Systems/ShootingEnemy.cs
+++ Assets/Scripts/Game Systems/ShootingEnemy.cs
@@ -8,24 +8,59 @@ public class ShootingEnemy : MonoBehaviour
     public float speed;
     public Transform target;
     public float minimumDistance;
+    bool canAttack;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Transform enemy;
 
 
     public GameObject projectile;
     public float timeBetweenShots;
     private float nextShotTime;
-
+    private void Start()
+    {
+        canAttack = true;
+        _rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > nextShotTime)
+        if(Time.time > nextShotTime && canAttack)
         {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            
-            nextShotTime = Time.time+ timeBetweenShots;
+            canAttack = false;
+            StartCoroutine(Attack());
         }
         if (Vector2.Distance(target.position, target.position) < minimumDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed* Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
         }
+        if (_rb.velocity.magnitude > 0)
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        if (_rb.velocity.x >= 0.01f)
+        {
+            enemy.localScale = new Vector3(-20f, 20f, 1f);
+
+        }
+        else if (_rb.velocity.x <= -0.01f)
+        {
+            enemy.localScale = new Vector3(20f, 20f, 1f);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        _animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(.7f);
+
+        Instantiate(projectile, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        canAttack = true;
+        nextShotTime = Time.time + timeBetweenShots; ;
+    }
+    public void TakeDamge()
+    {
+        _animator.SetTrigger("DMGTaken");
     }
 }
